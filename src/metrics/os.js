a7c71bdb76ff1861c51ba5c87a16aa0c1fbb60c2
Gustavo Alberola <alberolagustavo@gmail.com>
2015-03-04 11:45:40 -0300

    Moved cpu metrics from array of objects to separated array to be more compatible with ES

--- src/metrics/os.js
+++ src/metrics/os.js
@@ -12,6 +12,7 @@ var isMetaMetricEnabled;
 var tickTimeNs;
 var firstArrElement;
 var addSeparator;
+var cpuMatrix;
 
 function Module(options, metametrics) {
   if (!options) {
@@ -20,6 +21,28 @@ function Module(options, metametrics) {
 
   config = options;
   isMetaMetricEnabled = metametrics;
+
+  initCpuMatrix();
+}
+
+// The osCpuMatrix is a Matrix that contains the metrics for
+// osuser, osnice, ossys, osidle, osirq as first level
+// and the metric for the cpu number as second level
+//
+// Since the number of cores will not vary during the life of the app.
+// we create the matrix once and reuse the same object altering the
+// values on each iteration
+function initCpuMatrix() {
+  var amountOfMetricsPerCpu = 5;
+  // Init amout of metrics
+  cpuMatrix = new Array(amountOfMetricsPerCpu);
+  // Init amout of cores
+  tmp = os.cpus();
+  if (tmp && tmp.length > 0) {
+    for (x = 0, xMax = amountOfMetricsPerCpu; x < xMax ; x++) {
+      cpuMatrix[x] = new Array(tmp.length);
+    }
+  }
 }
 
 /**
@@ -47,31 +70,34 @@ Module.prototype.getMetric = function getMetric(writeStream) {
   ////////////////////
   // Track CPU Load //
   ////////////////////
+
+  // Note: ES doesn't get along very well with objects inside arrays,
+  // so we move the cpu metrics outside and make each of them a
+  // separated array
   tmp = os.cpus();
   if (tmp && tmp.length > 0) {
-    writeStream.write(',"oscpu":[');
-
-    firstArrElement = true;
+    // Populate matrix with each cpu metric
     for (x = 0, xMax = tmp.length; x < xMax ; x++) {
       cpu = tmp[x];
       if (cpu && (cpu = cpu.times)) {
-        // If not the first element, separate jsons with ,
-        if (!firstArrElement) {
-          writeStream.write(',');
-        }
-        writeStream.write('{' +
-                    '"oscore":' + x         + ',' +
-                    '"osuser":' + cpu.user  + ',' +
-                    '"osnice":' + cpu.nice  + ',' +
-                    '"ossys":'  + cpu.sys   + ',' +
-                    '"osidle":' + cpu.idle  + ',' +
-                    '"osirq":'  + cpu.irq   + ''  +
-                  '}');
-        // Notify that the next element is not the first one
-        firstArrElement = false;
+        cpuMatrix[0][x] = cpu.user;
+        cpuMatrix[1][x] = cpu.nice;
+        cpuMatrix[2][x] = cpu.sys;
+        cpuMatrix[3][x] = cpu.idle;
+        cpuMatrix[4][x] = cpu.irq;
+      } else {
+        // This should never happend, but if
+        // set all values for that index to 0
+        cpuMatrix[0][x] = cpuMatrix[1][x] = cpuMatrix[2][x] =
+        cpuMatrix[3][x] = cpuMatrix[4][x] = 0;
       }
     }
-    writeStream.write(']');
+
+    writeStream.write(',"osuser":[' + cpuMatrix[0] + ']');
+    writeStream.write(',"osnice":[' + cpuMatrix[1] + ']');
+    writeStream.write(',"ossys":['  + cpuMatrix[2] + ']');
+    writeStream.write(',"osidle":[' + cpuMatrix[3] + ']');
+    writeStream.write(',"osirq":['  + cpuMatrix[4] + ']');
   }
 
 
@@ -93,7 +119,6 @@ module.exports.keys = {
   'osfm': '',
   'osavg': '',
   'oscpu': '',
-  'oscore': '',
   'osuser': '',
   'osnice': '',
   'ossys': '',
